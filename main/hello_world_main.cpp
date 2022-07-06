7ddc5752f4f01dd636970f50f4a2015d4852d990
Kurosu Chan <crosstyan@192.168.1.5>
2022-07-06 17:21:46 +0800

    nimble init

---
+++ main/hello_world_main.cpp
@@ -0,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <memory>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_spi_flash.h"
+#include "Arduino.h"
+#include "NimBLEDevice.h"
+
+extern "C"{void app_main();}
+
+int scanTime = 5; //In seconds
+BLEScan* pBLEScan;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice* advertisedDevice) override {
+    printf("Advertised Device: %s \n", advertisedDevice->toString().c_str());
+  }
+};
+
+[[noreturn]] void scanTask (void * parameter){
+  for(;;) {
+    // put your main code here, to run repeatedly:
+    BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+    printf("Devices found: %d\n", foundDevices.getCount());
+    printf("Scan done!\n");
+    pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+    vTaskDelay(2000/portTICK_PERIOD_MS); // Delay a second between loops.
+  }
+
+  vTaskDelete(nullptr);
+}
+
+void app_main() {
+  initArduino();
+  printf("Hello world!\n");
+
+  NimBLEDevice::init("crosstyan");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+  xTaskCreate(scanTask, "scanTask", 5000, nullptr, 1, nullptr);
+}
