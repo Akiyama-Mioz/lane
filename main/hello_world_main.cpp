8d1f923232028f8aefa84d1437b09e651d877347
Kurosu Chan <crosstyan@192.168.1.5>
2022-07-07 12:06:09 +0800

    feat: print hex

--- main/hello_world_main.cpp
+++ main/hello_world_main.cpp
@@ -6,8 +6,10 @@
 
 // necessary for using fmt library
 #define FMT_HEADER_ONLY
+
 #include <cstdio>
 #include <functional>
+#include <vector>
 #include <memory>
 #include "fmt/core.h"
 #include "sdkconfig.h"
@@ -17,18 +19,40 @@
 #include "esp_spi_flash.h"
 #include "Arduino.h"
 #include "NimBLEDevice.h"
+#include "WiFi.h"
+#include <PubSubClient.h>
+#include <iomanip>
+#include <sstream>
 
 extern "C" { void app_main(); }
 
-const int scanTime = 5; //In seconds
-const int LoopInterval = 100;
+const char *ssid = "ilikong24g";
+const char *password = "1234567890";
+
+const char *mqtt_server = "23.249.16.149:1883";
+
+const int scanTime = 1; //In seconds
+const int LoopInterval = 50;
+
+std::string to_hex(const std::basic_string<char>& s) {
+  std::stringstream ss;
+  for (auto c: s) {
+    ss << std::hex << std::setfill('0') << std::setw(2) << (int) c;
+  }
+  return ss.str();
+}
 
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice *advertisedDevice) override {
 //    printf("Advertised Device: %s \n", advertisedDevice->toString().c_str());
-    fmt::print("Advertised Device: {}\n", advertisedDevice->toString());
+    if (advertisedDevice->getName().find("T03") != std::string::npos) {
+      fmt::print("Name: {}, Data: {}, RSSI: {}\n", advertisedDevice->getName(),
+                 to_hex(advertisedDevice->getManufacturerData()),
+                 advertisedDevice->getRSSI());
+    }
   }
 };
+
 
 [[noreturn]] void scanTask(BLEScan *pBLEScan) {
   for (;;) {
@@ -52,13 +76,13 @@ void app_main() {
   // the actual resource won't be released by RAII
   auto pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(false); //active scan uses more power, but get results faster
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setDuplicateFilter(false);
   pBLEScan->setFilterPolicy(BLE_HCI_SCAN_FILT_NO_WL);
   pBLEScan->setInterval(100);
   // Active scan time
   // less or equal setInterval value
-  // pBLEScan->setWindow(10);
+  pBLEScan->setWindow(99);
   xTaskCreate(reinterpret_cast<TaskFunction_t>(scanTask),
               "scanTask", 5000,
               static_cast<void *>(pBLEScan), 1,
