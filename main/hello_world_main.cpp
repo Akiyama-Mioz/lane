7fb0ea99ffdb8fb81cb4558fe1b21b7e3343f531
Kurosu Chan <crosstyan@192.168.1.5>
2022-07-07 15:19:37 +0800

    temp

--- main/hello_world_main.cpp
+++ main/hello_world_main.cpp
@@ -21,14 +21,14 @@
 
 extern "C" { void app_main(); }
 
-auto esp_name = "cross-esp";
+auto esp_name = "e-track 011";
 
 const int scanTime = 1; //In seconds
 const int LoopInterval = 50;
 // string is array as well
 const int capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(62);
 
-std::string to_hex(const std::basic_string<char>& s) {
+std::string to_hex(const std::basic_string<char> &s) {
   std::string res;
   for (auto c: s) {
     res += fmt::format("{:02x}", c);
@@ -44,7 +44,7 @@ class AdCallback : public BLEAdvertisedDeviceCallbacks {
       doc["name"] = advertisedDevice->getName();
       doc["rssi"] = advertisedDevice->getRSSI();
       auto arr = doc.createNestedArray("data");
-      for(auto c: advertisedDevice->getManufacturerData()) {
+      for (auto c: advertisedDevice->getManufacturerData()) {
         arr.add(c);
       }
       serializeJson(doc, output);
@@ -67,10 +67,33 @@ class AdCallback : public BLEAdvertisedDeviceCallbacks {
   vTaskDelete(nullptr);
 }
 
+const char *service_uuid = "00001801-0000-1000-8000-00805f9b34fb";
+const char *characteristic_uuid = "00002a00-0000-1000-8000-00805f9b34fb";
+
 void app_main() {
   initArduino();
 
   NimBLEDevice::init(esp_name);
+//  NimBLEDevice::setPower(ESP_PWR_LVL_P9);
+  NimBLEDevice::setPower(ESP_PWR_LVL_N3);
+  NimBLEDevice::setSecurityIOCap(BLE_HS_IO_NO_INPUT_OUTPUT);
+
+
+  auto pServer = NimBLEDevice::createServer();
+  auto pService = pServer->createService(service_uuid);
+  auto pCharacteristic = pService->createCharacteristic(
+      characteristic_uuid,
+      NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE
+  );
+  pCharacteristic->setValue("233");
+//  pServer->start();
+
+  auto pAdvertising = NimBLEDevice::getAdvertising();
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setName(esp_name);
+  pAdvertising->setAppearance(1154);
+  pAdvertising->start();
+
   // return a pointer
   // the actual resource won't be released by RAII
   auto pBLEScan = BLEDevice::getScan();
@@ -82,6 +105,7 @@ void app_main() {
   // Active scan time
   // less or equal setInterval value
   pBLEScan->setWindow(99);
+
   xTaskCreate(reinterpret_cast<TaskFunction_t>(scanTask),
               "scanTask", 5000,
               static_cast<void *>(pBLEScan), 1,
